9ba97a0abaca949edc40fa7d9562be94ea9e5455
mhmdyouali <mhmd.you@yahoo.com>
2022-11-18 13:52:55 +0200

    @PointCut

--- src/com/spring/aop/DataBase.java
+++ src/com/spring/aop/DataBase.java
@@ -2,14 +2,25 @@ package com.spring.aop;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
  
 @Aspect
 @Component
 public class DataBase {
 	
-	@Before("execution(public * com.spring.dao.*.*(..))") 
+	@Pointcut("execution(public * com.spring.dao.*.*(..))") 
+	public void myPointCut() {
+		
+	}
+	
+	@Before("myPointCut()") 
 	public void Connection() {
 		System.out.println("Connected !!");
+	}
+	
+	@Before("myPointCut()") 
+	public void login() {
+		System.out.println("login success !!!");
 	}
 }
